52d60df9ddf9abcaba24528416a18b0cd69b37ee
Lucas Notebook Casa <lucasandrade730@gmail.com>
2023-08-27 22:24:14 -0300

    feat: Continue implementation

--- ReflowOven.API.Integration.Peripheral/HostedServices/BackgroundWorkerService.cs
+++ ReflowOven.API.Integration.Peripheral/HostedServices/BackgroundWorkerService.cs
@@ -1,3 +1,6 @@
+using System.Device.Gpio;
+using System.Net.NetworkInformation;
+
 namespace ReflowOven.API.Integration.Peripheral.HostedServices;
 
 public class BackgroundWorkerService : BackgroundService
@@ -10,11 +13,22 @@ public class BackgroundWorkerService : BackgroundService
     }
 
 
-
+    bool ledOn = true;
     protected async override Task ExecuteAsync(CancellationToken stoppingToken)
     {
          while(!stoppingToken.IsCancellationRequested)
         {
+            const int Pin1 = 6;
+            const int Pin2 = 13;
+            using var controller = new GpioController();
+
+            controller.OpenPin(Pin1, PinMode.Output);
+            controller.OpenPin(Pin2, PinMode.Output);
+
+            controller.Write(Pin1, ((ledOn) ? PinValue.Low : PinValue.High));
+            controller.Write(Pin2, ((ledOn) ? PinValue.High : PinValue.Low));
+            ledOn = !ledOn;
+
             _logger.LogInformation("Worker running at:{time}",DateTimeOffset.Now);
            await Task.Delay(1000,stoppingToken);
         }
