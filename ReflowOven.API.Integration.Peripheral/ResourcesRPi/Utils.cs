e994ae44868edaa71dd8e0cfe1e7ac0997ab49d9
Lucas Silva <lucas.silva@jassy.ag>
2023-09-19 12:28:15 -0300

    included Utils.cs

---
+++ ReflowOven.API.Integration.Peripheral/ResourcesRPi/Utils.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+
+namespace ReflowOven.API.Integration.Peripheral.ResourcesRPi;
+
+public class Utils
+{
+
+    public static byte[] SerializeToBytes<T>(T obj)
+    {
+        return JsonSerializer.SerializeToUtf8Bytes(obj);
+    }
+
+    public static T DeserializeFromBytes<T>(byte[] bytes)
+    {
+        return JsonSerializer.Deserialize<T>(bytes)!;
+    }
+}
